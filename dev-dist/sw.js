eda44d8c5f38d9fff0236e6679e301fa30f65f75
Emre <emreisik20@gmail.com>
2026-02-13 01:45:17 +0300

    FINAL FIX: Build script simplified - no npm ci in build
    - preinstall handles backend deps
    
    - build only runs compile commands
    
    Railway cache invalidated

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-137dedbd'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.0gb56tme9dg"
+    "revision": "0.bfk1e075so"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
